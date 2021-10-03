7bea9cbfc7a98db04e7476e677971abebfba718b
sriram <sriramna18102@it.ssn.edu.in>
2021-10-03 15:25:00 +0530

    Deploying commit

---
+++ build/precache-manifest.ecad2d750d12cc7d1ce0f3f9b1a159ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56c7cad688805faa2d2cc57c7366097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c45edaf53f82d8fcacaa",
+    "url": "/static/css/main.cf566840.chunk.css"
+  },
+  {
+    "revision": "18c46870410028aa4fe2",
+    "url": "/static/js/2.26fe6cee.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.26fe6cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45edaf53f82d8fcacaa",
+    "url": "/static/js/main.dd6ca11e.chunk.js"
+  },
+  {
+    "revision": "a93c4335e4ebfb57963d",
+    "url": "/static/js/runtime-main.1235aae3.js"
+  },
+  {
+    "revision": "d8b13636d1a7a07cf81d61fec3440bdd",
+    "url": "/static/media/homeback.d8b13636.jpg"
+  },
+  {
+    "revision": "e65ca72257aa9beeedee87f081c3483c",
+    "url": "/static/media/marvel.e65ca722.gif"
+  }
+]);
